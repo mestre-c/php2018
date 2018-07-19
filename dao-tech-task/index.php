c19dccca3aa38dda95bc633ac3401da7f0f38a29
capfer27@gmail.com <Capfer12@hotmail.com>
2018-07-19 18:02:22 +0400

    initial commit

---
+++ dao-tech-task/index.php
@@ -0,0 +1,23 @@
+<?php 
+	
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Dijkstra's Algorithm</title>
+</head>
+<body>
+		<h1>Find the shortest path</h1>
+		<form action="shortest.php" method="GET">
+			<fieldset>
+				<legend>Find The Shortest Path - Traveling Salesman Problem</legend>
+				
+			</fieldset>
+			
+		</form>
+</body>
+</html>
